9b885739d02a69ed91f24ceec62e61d865dc8d21
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-10-24 18:41:05 +0530

    Add project files.

---
+++ SectionC_1.14Sub/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SectionC_1._14Sub
+{
+    
+    
+
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            string s=Directory.GetCurrentDirectory();
+            Console.WriteLine(s);
+            try
+                {
+                string filePath = s+ "\\Student_data.txt";
+                
+
+                FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Read);
+
+                StreamReader sr = new StreamReader(fs);
+                    string data = sr.ReadToEnd();
+                    Console.WriteLine(data);
+
+               
+            
+
+
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
